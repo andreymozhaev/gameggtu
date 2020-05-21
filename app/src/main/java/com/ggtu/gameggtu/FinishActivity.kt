8f59d35dfd8ab5b763dbb5b8d8ee20efbf765aad
Andrey <dedok-iu3@yandex.ru>
2020-05-21 18:30:35 +0300

    Lesson 1

---
+++ app/src/main/java/com/ggtu/gameggtu/FinishActivity.kt
@@ -0,0 +1,12 @@
+package com.ggtu.gameggtu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+    }
+}
